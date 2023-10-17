0c25672b019e7e7b45533c21161459ea1c389c95
im-Starry <s2200054@student.unklab.ac.id>
2023-10-17 18:53:13 +0800

    Translate QuestionCard

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable prefer-template */
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 /* Card untuk tiap pertanyaan, akan menampilkan author question, judul dan pertanyaan,
    kapan question dibuat, dan jumlah balasan tiap question */
@@ -9,13 +9,13 @@ export default function QuestionCard({ item }) {
   const renderTag = () => {
     if (item.tags.length === 0) {
       return (
-        <span className="py-1 px-2 text-sm bg-primary-1 rounded-md text-white">
-          Belum ada tag
+        <span className='py-1 px-2 text-sm bg-primary-1 rounded-md text-white'>
+          No Tag
         </span>
       );
     }
     const tags = item.tags.map((tag) => (
-      <span className="py-1 px-2 text-sm bg-primary-1 rounded-md text-white">
+      <span className='py-1 px-2 text-sm bg-primary-1 rounded-md text-white'>
         {tag}
       </span>
     ));
@@ -32,40 +32,40 @@ export default function QuestionCard({ item }) {
     const diffInMonths = Math.floor(diffInDays / 30);
     const diffInYears = Math.floor(diffInMonths / 12);
     if (diffInMinutes < 60) {
-      return `${diffInMinutes} menit yang lalu`;
+      return `${diffInMinutes} Minute ago`;
     }
     if (diffInHours < 24) {
-      return `${diffInHours} jam yang lalu`;
+      return `${diffInHours} Hour ago`;
     }
     if (diffInDays < 30) {
-      return `${diffInDays} hari yang lalu`;
+      return `${diffInDays} Day ago`;
     }
     if (diffInMonths < 12) {
-      return `${diffInMonths} bulan yang lalu`;
+      return `${diffInMonths} Month ago`;
     }
-    return `${diffInYears} tahun yang lalu`;
+    return `${diffInYears} Year ago`;
   };
 
   const renderReply = () => {
     if (item.replies.length === 0) {
       return (
-        <span className="text-primary-1 font-medium text-sm">
-          Belum ada Pembahasan
+        <span className='text-primary-1 font-medium text-sm'>
+          No Discussion
         </span>
       );
     }
     if (item.replies.length === 1) {
       return (
-        <span className="text-primary-1 font-medium text-sm">
+        <span className='text-primary-1 font-medium text-sm'>
           {item.replies.length}
-          Pembahasan
+          Discussion
         </span>
       );
     }
     return (
-      <span className="text-primary-1 font-medium text-sm">
+      <span className='text-primary-1 font-medium text-sm'>
         {`${item.replies.length} `}
-        Pembahasan
+        Discussion
       </span>
     );
   };
@@ -73,46 +73,44 @@ export default function QuestionCard({ item }) {
   return (
     <Link
       to={`/question/detail/${item._id}`}
-      className="flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start mt-3 bg-white shadow-lg border-2 rounded-lg p-3 hover:bg-slate-50"
-    >
-      <div className="lg:w-3/4 mt-3 lg:mt-0 text-center lg:text-start">
-        <h3 className="font-semibold text-xl mb-3">{item.title}</h3>
-        <p className="lg:w-3/4">
+      className='flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start mt-3 bg-white shadow-lg border-2 rounded-lg p-3 hover:bg-slate-50'>
+      <div className='lg:w-3/4 mt-3 lg:mt-0 text-center lg:text-start'>
+        <h3 className='font-semibold text-xl mb-3'>{item.title}</h3>
+        <p className='lg:w-3/4'>
           {item.description.length > 100
-            ? item.description.substring(0, 100) + '...'
+            ? item.description.substring(0, 100) + "..."
             : item.description}
           {item.description.length > 100 && (
             <span
               href={`/question/detail/${item._id}`}
-              className="text-primary-1 font-semibold"
-            >
-              Baca selengkapnya
+              className='text-primary-1 font-semibold'>
+              Read more..
             </span>
           )}
         </p>
-        <p className="mt-2 text-primary-2 text-sm">
+        <p className='mt-2 text-primary-2 text-sm'>
           {` ${item.username_author}`}
         </p>
       </div>
       <div>
-        <div className="flex items-center mb-1">
+        <div className='flex items-center mb-1'>
           {/* <img src={avatar} alt="" className="w-12 h-12" /> */}
-          <div className="flex flex-col">
+          <div className='flex flex-col'>
             {/* <span className="font-semibold">John Doe</span> */}
-            <div className="flex items-center">
-              <i className="fa-solid fa-clock text-primary-1 mr-2" />
-              <span className="text-sm">{renderTime()}</span>
+            <div className='flex items-center'>
+              <i className='fa-solid fa-clock text-primary-1 mr-2' />
+              <span className='text-sm'>{renderTime()}</span>
             </div>
           </div>
         </div>
         <div>
-          <div className="mb-1">
-            <i className="fa-solid fa-message text-primary-1 mr-2" />
+          <div className='mb-1'>
+            <i className='fa-solid fa-message text-primary-1 mr-2' />
             {renderReply()}
           </div>
-          <div className="flex w-52">
-            <i className="fa-solid fa-tag text-primary-1 mr-2" />
-            <div className="flex gap-2 flex-wrap">{renderTag()}</div>
+          <div className='flex w-52'>
+            <i className='fa-solid fa-tag text-primary-1 mr-2' />
+            <div className='flex gap-2 flex-wrap'>{renderTag()}</div>
           </div>
         </div>
       </div>
